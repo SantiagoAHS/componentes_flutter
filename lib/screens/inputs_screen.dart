bfb9b1cae87974d0fae815c29fd123a755f52085
SantiagoAHS <lionelsanti2018@gmail.com>
2024-03-07 12:03:03 -0600

    21 Funtion ButtonNavigatorBar

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/screens/home_screen.dart';
+import 'package:practica3/screens/images_screen.dart';
+import 'package:practica3/screens/infinite_list_screen.dart';
+import 'package:practica3/screens/notifications_screen.dart';
 import 'package:practica3/theme/app_theme.dart';
 
 class Inputsscreen extends StatefulWidget {
@@ -16,6 +20,34 @@ class _InputsscreenState extends State<Inputsscreen> {
   bool selectedCheckBoxOption1 = false;
   bool selectedCheckBoxOption2 = false;
   bool selectedCheckBoxOption3 = false;
+  List screens =const [
+    HomeScreen(),
+    InfiniteListScreen(),
+    NotificationsScreen(),
+    ImagesScreen(),
+  ];
+
+  openScreen(int index){
+    setState(() {
+      MaterialPageRoute ruta= MaterialPageRoute(builder: (context)=> const HomeScreen());
+      switch(index){
+        case 0:ruta = MaterialPageRoute(builder: (context)=> const HomeScreen());
+          break;
+        case 1:ruta =MaterialPageRoute(builder: (context)=> const InfiniteListScreen());
+          break;
+        case 2:ruta = MaterialPageRoute(builder: (context)=> const NotificationsScreen());
+          break;
+        case 3:ruta =MaterialPageRoute(builder: (context)=> const ImagesScreen());
+          break;
+    }
+      selectedIndex = index;
+      Navigator.push(
+        context, 
+        ruta
+        );
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -43,11 +75,15 @@ class _InputsscreenState extends State<Inputsscreen> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex : selectedIndex,
-        backgroundColor: const Color.fromARGB(255, 38, 160, 160),
+        backgroundColor: const Color.fromARGB(255, 16, 255, 255),
+        unselectedItemColor: AppTheme.mainColor,
+        onTap: (index) => openScreen(index),
         items: const[
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.black87,), label: "Inicio",),
-          BottomNavigationBarItem(icon: Icon(Icons.data_object,color: Colors.black87,), label: "Datos"),
-          BottomNavigationBarItem(icon: Icon(Icons.exit_to_app, color: Colors.black87,), label: "Salida")
+          BottomNavigationBarItem(icon: Icon(Icons.home, color: Color.fromARGB(221, 2, 0, 0),), label: "Inicio",),
+          BottomNavigationBarItem(icon: Icon(Icons.data_object,color: Colors.black87,), label: "List"),
+          BottomNavigationBarItem(icon: Icon(Icons.notification_add_outlined, color: Colors.black87,), label: "Noti"),
+          BottomNavigationBarItem(icon: Icon(Icons.image_search_outlined, color: Colors.black87,), label: "Imgs"),
+          BottomNavigationBarItem(icon: Icon(Icons.exit_to_app, color: Colors.black87,), label: "Salida",)
         ],
         unselectedLabelStyle: AppTheme.lightTheme.textTheme.headlineSmall,
       ),
